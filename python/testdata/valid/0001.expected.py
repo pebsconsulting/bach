d1e3e3bb3f60f2c691ca4c077c1675de2f4ea660
Ben Sarah Golightly <golightly.ben@googlemail.com>
2017-09-11 12:07:28 +0100

    Update test

--- python/testdata/valid/0001.expected.py
+++ python/testdata/valid/0001.expected.py
@@ -1,4 +1,4 @@
-('document', {'attribute': ['value']}, [
-    ('subdocument', {'attribute': ['value one', 'value two']}, [
+('document', {'attribute': 'value'}, [
+    ('subdocument', {'attribute': 'value one value two'}, [
         'literal one', 'literal two', 'literal with some £¹²³ unicode'])
 ])
